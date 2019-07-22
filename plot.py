ab6b38a54d04097a2924330c7730be8f339ab9a1
Amaury Chamayou <achamayou@gmail.com>
2019-07-22 17:29:17 +0100

    Update

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
 import yaml
-from pymongo import MongoClient
+import pymongo
 import os
 
 def get_settings():
@@ -9,13 +9,31 @@ def get_settings():
 
 DB, COLLECTION, MAIN_BRANCH = get_settings()
 
+class Metrics:
+  def __init__(self):
+    uri = os.getenv("COSMOS_CONNECTION")
+    self.client = pymongo.MongoClient(uri)
+    db = self.client[DB]
+    self.col = db[COLLECTION]
+
+  def all(self):
+    return self.col.find()
+
+  def last_for_branch(self, branch): 
+    return next(self.col.find({'branch': branch}).sort([('created', pymongo.DESCENDING)]).limit(1))
+
+  def bars(self, branch, reference):
+    branch_metrics = self.last_for_branch(branch)['metrics']
+    reference_metrics = self.last_for_branch(reference)['metrics']
+
+    b = []
+    r = []
+    ticks = []
+    for field in branch_metrics.keys() | reference_metrics.keys():
+      b = branch_metrics.get
+ 
 if __name__ == '__main__':
-  uri = os.getenv("COSMOS_CONNECTION")
-  mongo_client = MongoClient(uri)
- 
-  db = mongo_client[DB]
-  col = db[COLLECTION]
-  docs = col.find({})
- 
-  for doc in docs:
-    print(doc)
+  m = Metrics()
+  #for p in m.all():
+  #  print(p)
+  print(next(m.last_for_branch(MAIN_BRANCH)))
