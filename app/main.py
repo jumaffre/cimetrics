603573b9d0e46f43d4763e1bdd06f46d41a34e19
Amaury Chamayou <achamayou@gmail.com>
2019-07-23 14:16:51 +0100

    more metrics

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ import pymetrics
 
 metrics = pymetrics.Metrics()
 
-metrics.put('Signed throughput', random.random(100_0000))
-metrics.put('Unsigned throughput', random.random(100_000))
+metrics.put('Signed throughput', random.randint(50_000, 100_0000))
+metrics.put('Unsigned throughput', random.randint(50_000, 100_000))
 metrics.put('Local latency', random.randint(10, 100))
 metrics.put('Global latency', random.randint(10, 100))
 metrics.put('Geo-replicated latency', random.randint(10, 100))
