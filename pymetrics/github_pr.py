2dd9a5214c0159fa4b065bfdc6393a678d3afd2d
Julien Maffre <jumaffre@microsoft.com>
2019-07-23 10:38:33 +0100

    Publish comment to GitHub PR with report

--- pymetrics/github_pr.py
+++ pymetrics/github_pr.py
@@ -1,39 +1,70 @@
 import requests
 import json
 import sys
+import base64
+import datetime
+import os
 
-TOKEN = "0b0cbbcd28e5c4902c00b58614eac1bb952f71e3"
+# To get from the CI environment variables
 USERNAME = "jumaffre"
+REPO = "jumaffre/metrics-devops"
 
-REQUEST_HEADERS = {'content-type': 'application/json', 'Authorization': 'token {}'.format(TOKEN)}
-GITHUB_URL = "https://api.github.com/repos/jumaffre/metrics-devops/git/refs"
+# Always the same for metrics-devops
+IMAGE_BRANCH_NAME = "metrics-devops"
 
-IMAGE_BRANCH_NAME = "images22"
-REQUEST_CREATE_BRANCH = {"ref": "refs/heads/images22333", "sha": "ab6b38a54d04097a2924330c7730be8f339ab9a1"}
+def get_github_token():
+  return os.environ['GITHUB_TOKEN']
 
+# REST specific
+REQUEST_HEADERS = {'content-type': 'application/json', 'Authorization': 'token {}'.format(get_github_token())}
+GITHUB_URL = f"https://api.github.com/repos/{REPO}"
+BRANCH_CREATION_URL = f"{GITHUB_URL}/git/refs"
+IMAGE_UPLOADING_URL = f"{GITHUB_URL}/contents/{IMAGE_BRANCH_NAME}/image{datetime.datetime.now()}.png"
 
 # Create branch to push images to 
 def create_branch():
     # Does not do anything if the branch already exists
-    print(f"Creating branch {REQUEST_CREATE_BRANCH}, head={REQUEST_HEADERS}")
-
     params = {}
-    params["ref"] = "refs/heads/images22333"
+    params["ref"] = f"refs/heads/{IMAGE_BRANCH_NAME}"
+
+    # TODO: Should get the hash of master instead?
     params["sha"] = "ab6b38a54d04097a2924330c7730be8f339ab9a1"
 
-    rep = requests.post(GITHUB_URL, data=json.dumps(params), headers=REQUEST_HEADERS)
+    print(f"Creating branch {json.dumps(params)}")
+    rep = requests.post(BRANCH_CREATION_URL, data=json.dumps(params), headers=REQUEST_HEADERS)
     print(rep.text)
+
+def upload_image(encoded_image):
+    params = {}
+    params["message"] = "Uploading an image"
+    params["branch"] = IMAGE_BRANCH_NAME
+    params["content"] = encoded_image
+
+    print(f"Uploading image to branch {IMAGE_BRANCH_NAME}")
+    rep = requests.put(IMAGE_UPLOADING_URL, data=json.dumps(params), headers=REQUEST_HEADERS)
+    json_rep = json.loads(rep.text)
+    return json_rep["content"]["download_url"]
+
+def publish_comment(pull_request_id, image_report_url):
+    params = {}
+    params["body"] = f"Performance report: \n ![images]({image_report_url}) \n **Have a look before merging** :bar_chart: :eyes: :thumbsup:"
+
+    print(f"Publishing comment to pull request {pull_request_id}")
+    rep = requests.post(f"{GITHUB_URL}/issues/{str(pull_request_id)}/comments", data=json.dumps(params), headers=REQUEST_HEADERS)
 
 
 if __name__ == '__main__':
-    print("Hello there")
+    # Try to create the branch to upload images to
     create_branch()
 
-    # For now, load image
+    encoded_image = None
+    image_path = "_pymetrics/diff.png"
+    with open(image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read())
 
-    # TODO: 
-    #   0. Get image
-    #   1. Turn image into Base64 encoded version
-    #   2. Upload image
-    #   3. Public comment
+    # Upload base64 image to dedicated branch
+    image_url = upload_image(str(encoded_image.decode()))
 
+    # Comment on the pull request
+    pull_request_id = 1
+    publish_comment(pull_request_id, image_url)
