6d37a1fc79eded9129b83960a5ddb66d18634b8a
Amaury Chamayou <amchamay@microsoft.com>
2019-07-24 11:49:12 +0100

    Relative values

--- cimetrics/plot.py
+++ cimetrics/plot.py
@@ -30,8 +30,7 @@ class Metrics(object):
     branch_metrics = self.last_for_branch(branch)['metrics']
     reference_metrics = self.last_for_branch(reference)['metrics']
 
-    b = []
-    r = []
+    b, r = [], []
     ticks = []
     for field in branch_metrics.keys() | reference_metrics.keys():
       b.append(branch_metrics.get(field, {}).get('value', 0))
@@ -39,6 +38,9 @@ class Metrics(object):
       ticks.append(field)
 
     return b, r, ticks
+
+  def normalise(self, new, ref):
+    return [n / r for n, r in zip(new, ref)]
 
 if __name__ == '__main__':
   env = get_env()
@@ -50,14 +52,15 @@ if __name__ == '__main__':
     target_branch = env.target_branch
     print(f"Comparing {BRANCH} and {target_branch}")
     branch, main, ticks = m.bars(BRANCH, target_branch)
+    values = m.normalise(branch, main)
     fig, ax = plt.subplots()
     index = np.arange(len(ticks))
     bar_width = 0.35
     opacity = 0.9
-    ax.bar(index, branch, bar_width, alpha=opacity, color='r',
+    ax.bar(index, values, bar_width, alpha=opacity, color='r',
           label=BRANCH)
-    ax.bar(index+bar_width, main, bar_width, alpha=opacity, color='b',
-          label=target_branch)
+    ax.bar(index+bar_width, [1] * len(ticks), bar_width, alpha=opacity, color='b',
+           label=target_branch)
     ax.set_xlabel('Metrics')
     ax.set_ylabel('Value')
     ax.set_title(f"{BRANCH} vs {target_branch}")
