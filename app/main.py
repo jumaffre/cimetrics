da0c599746df7660d8f8df8ecabdaa731c3908a3
Julien Maffre <jumaffre@microsoft.com>
2019-07-24 16:50:16 +0100

    More metrics

--- app/main.py
+++ app/main.py
@@ -7,13 +7,21 @@ def run_benchmark():
     return {
         'throughput': 50000,
         'latency': 10,
-        'peak_wss': 50
+        'peak_wss': 50,
+        'accuracy': 0.7,
+        'error_rate': 0.05,
+        'memory_fragmentation': 0.02,
+        'cpu_usage': 0.30
     }
 
 results = run_benchmark()
 
 metrics = cimetrics.upload.Metrics()
-metrics.put('Throughput', results['throughput'])
-metrics.put('Latency', results['latency'])
-metrics.put('Peak working set size', results['peak_wss'])
+metrics.put('Throughput (tx/s)', results['throughput'])
+metrics.put('Latency (ms)', results['latency'])
+metrics.put('Peak working set size (GB)', results['peak_wss'])
+metrics.put('Accuracy (%)', results['accuracy'])
+metrics.put('Error rate (%)', results['error_rate'])
+metrics.put('Memory fragmentation (%)', results['memory_fragmentation'])
+metrics.put('CPU usage (%)', results['cpu_usage'])
 metrics.publish()
