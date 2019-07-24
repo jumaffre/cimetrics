3305ea0c78f8f01d1486e7c27c333b303f6c4830
Maik Riechert <maik.riechert@microsoft.com>
2019-07-24 10:16:10 +0100

    packaging

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # metrics-devops
 
 [![Build Status](https://dev.azure.com/jumaffre/metrics-devops/_apis/build/status/jumaffre.metrics-devops?branchName=master)](https://dev.azure.com/jumaffre/metrics-devops/_build/latest?definitionId=1&branchName=master)
+
+## Development
+
+Install as editable/dev package:
+```sh
+pip install -e .
+```
+
+Submit sample metrics and create plots:
+```sh
+export METRICS_MONGO_CONNECTION=...
+python app/main.py
+python -m pymetrics.plot
+ls _pymetrics
+```
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pymetrics',
+    version='0.1.0',
+    description='TBD',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jumaffre/metrics-devops',
+    author='Julien Maffre',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['pymetrics'],
+    python_requires='>=3.3',
+    install_requires=[
+        'pymongo',
+        'pyyaml',
+        'gitpython',
+        'requests',
+        'matplotlib',
+        'numpy',
+    ]
+)
