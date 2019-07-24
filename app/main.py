b51e89e0c4eb9aaf9f91395df00c962df7fcec5a
Maik Riechert <maik.riechert@microsoft.com>
2019-07-24 15:27:14 +0100

    fix app

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ import cimetrics.upload
 
 def run_benchmark():
     return {
-        'throughput': 50000,
-        'latency': 10,
-        'peak_wss': 50
+        'throughput': 50500,
+        'latency': 9,
+        'peak_wss': 47
     }
 
 results = run_benchmark()
