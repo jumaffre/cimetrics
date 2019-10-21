e3b33d6219ea3c6f074c4b4f087374222a8c5fce
Julien Maffre <jumaffre@microsoft.com>
2019-10-21 22:18:23 +0100

    Real life scenario

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cimetrics",
-    version="0.2.0",
+    version="0.2.1-alpha.1",
     description="Lightweight python module to track crucial metrics",
     url="https://github.com/jumaffre/cimetrics",
     author="Julien Maffre",
