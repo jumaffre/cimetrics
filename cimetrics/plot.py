6bf542f3de63edb7653d44746320cc23a2049d5b
Amaury Chamayou <amchamay@microsoft.com>
2020-04-14 14:29:08 +0100

    .

--- cimetrics/plot.py
+++ cimetrics/plot.py
@@ -144,7 +144,8 @@ def trend_view(env, tgt_only=False):
         ncol = 1
         fsize = matplotlib.figure.figaspect(env.columns)
         dpi_adjust = fsize[1] / matplotlib.rcParams["figure.figsize"][1]
-        matplotlib.rcParams["axes.titlesize"] = 4
+        matplotlib.rcParams["axes.titlesize"] = 6
+        matplotlib.rcParams["font.size"] = 5
         fig = plt.figure(figsize=fsize)
     else:
         branch_series = m.branch_history(env.branch, env.build_id)
