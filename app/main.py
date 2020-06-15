0292b2133d8ef968e573701eb0a2a3be749c83fd
Amaury Chamayou <amaury@xargs.fr>
2020-06-15 13:26:52 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,8 +20,3 @@ with cimetrics.upload.metrics() as m:
     m.put("Throughput (tx/s) ^", results["throughput"])
     m.put("Latency (ms)", results["latency"])
     m.put("Peak working set size (GB)", results["peak_wss"])
-    m.put("Accuracy (%) ^", results["accuracy"])
-    m.put("Error rate (%)", results["error_rate"])
-    m.put("Memory fragmentation (%)", results["memory_fragmentation"])
-    m.put("CPU usage (%)", results["cpu_usage"])
-    m.put("New metric (U)", results["new_metric"])
