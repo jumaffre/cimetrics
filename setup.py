8dcfd7bf428ea2d838312fcc33b7e11eb7446db5
Amaury Chamayou <amchamay@microsoft.com>
2021-02-05 10:29:17 +0000

    .

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cimetrics",
-    version="0.3.0",
+    version="0.3.1",
     description="Lightweight python module to track crucial metrics",
     long_description=long_description,
     long_description_content_type='text/markdown',
