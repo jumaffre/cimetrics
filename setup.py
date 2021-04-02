695401fb2c70273a3f67955160cbf6606cefbfef
Amaury Chamayou <amaury@xargs.fr>
2021-04-02 15:18:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cimetrics",
-    version="0.3.3",
+    version="0.3.4",
     description="Lightweight python module to track crucial metrics",
     long_description=long_description,
     long_description_content_type="text/markdown",
