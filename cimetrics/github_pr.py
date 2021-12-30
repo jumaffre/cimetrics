2ff7534617ab6cc0bdf98972a7a702e86cb87c0e
Amaury Chamayou <amaury@xargs.fr>
2021-12-30 14:43:47 +0000

    Remove GitHub branch image publishing (#128)

--- cimetrics/github_pr.py
+++ cimetrics/github_pr.py
@@ -12,7 +12,6 @@ from azure.storage.blob import BlobServiceClient, ContentSettings
 from cimetrics.env import get_env
 
 # Always the same for metrics-devops
-IMAGE_BRANCH_NAME = "cimetrics"
 IMAGE_PATH = "_cimetrics/diff.png"
 COMMENT_PATH = "_cimetrics/diff.txt"
 
@@ -32,43 +31,6 @@ class GithubPRPublisher(object):
         }
         self.github_url = f"https://api.github.com/repos/{self.env.repo_id}"
         self.pull_request_id = self.env.pull_request_id
-
-    def create_image_branch(self):
-        # Does not do anything if the branch already exists
-        params = {}
-        params["ref"] = f"refs/heads/{IMAGE_BRANCH_NAME}"
-        rep = requests.get(
-            f"{self.github_url}/git/refs/heads/main",
-            data="",
-            headers=self.request_header,
-        )
-        json_rep = json.loads(rep.text)
-        params["sha"] = json_rep["object"]["sha"]
-
-        print(f"Creating branch {json.dumps(params)}")
-        rep = requests.post(
-            f"{self.github_url}/git/refs",
-            data=json.dumps(params),
-            headers=self.request_header,
-        )
-
-    def upload_image(self, encoded_image):
-        params = {}
-        params["message"] = "Uploading an image"
-        params["branch"] = IMAGE_BRANCH_NAME
-        params["content"] = encoded_image
-
-        print(f"Uploading image to branch {IMAGE_BRANCH_NAME}")
-        rep = requests.put(
-            f"{self.github_url}/contents/{IMAGE_BRANCH_NAME}/image{datetime.datetime.now()}.png",
-            data=json.dumps(params),
-            headers=self.request_header,
-        )
-        json_rep = json.loads(rep.text)
-        if "content" in json_rep:
-            return json_rep["content"]["download_url"]
-        else:
-            raise Exception("Failed to upload image")
 
     def upload_image_as_blob(self, contents):
         service = BlobServiceClient(account_url=AZURE_BLOB_URL)
@@ -127,8 +89,6 @@ if __name__ == "__main__":
 
     publisher = GithubPRPublisher()
 
-    publisher.create_image_branch()
-
     encoded_image = None
     raw_image = None
     with open(os.path.join(env.repo_root, IMAGE_PATH), "rb") as image_file:
@@ -138,8 +98,8 @@ if __name__ == "__main__":
     with open(os.path.join(env.repo_root, COMMENT_PATH), "r") as comment_file:
         comment = comment_file.read()
 
-    if AZURE_BLOB_URL and AZURE_WEB_URL:
-        image_url = publisher.upload_image_as_blob(raw_image)
-    else:
-        image_url = publisher.upload_image(str(encoded_image.decode()))
+    assert (
+        AZURE_BLOB_URL and AZURE_WEB_URL
+    ), "Either AZURE_BLOB_URL or AZURE_WEB_URL is not set"
+    image_url = publisher.upload_image_as_blob(raw_image)
     publisher.publish_comment(image_url, comment)
